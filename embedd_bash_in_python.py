762a4587e1e0e5368604010cebee31c9ec96af50
Michael <silent-mobius@users.noreply.github.com>
2015-01-14 12:18:18 +0200

    Create embedd_bash_in_python.py
    basic example of embedding bash commands in  python

---
+++ embedd_bash_in_python.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+################################################################
+#Created by: michael 'tuxthepenguin'
+#           for linuxsystems.rocks
+#
+################################################################
+
+#importing libs
+import os
+import subprocess
+
+def bash(cmd):
+    os.system('cmd')
+  
+
+def sh(cmd):
+   os.system('bash -c %s' %script)
+
